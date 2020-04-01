b0a5641b44489d9df467a33ea71dd9e4d5d3ee00
Kyoc0de <kyoscode@gmail.com>
2020-04-02 05:06:23 +0800

    项目初始创建

---
+++ mall-coupon/src/test/java/com/kyo/mall/coupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyo.mall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
